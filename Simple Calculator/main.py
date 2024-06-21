9cd19390622add9ffb178d21d3eceaa0284a39eb
Prateek Agrawal <138208959+Prateek0305@users.noreply.github.com>
2024-06-21 19:42:05 +0530

    Update main.py

--- Simple Calculator/main.py
+++ Simple Calculator/main.py
@@ -1,1 +1,19 @@
-
+first=input("Enter first number:")
+operator=input("Enter any operator(+,-,*,%,/,**):")
+second=input("Enter second number")
+a=int(first)
+b=int(second)
+if operator=='+':
+    print(a+b)
+elif operator=='-':
+    print(a-b)
+elif operator=='*':
+    print(a*b)
+elif operator=='/':
+    print(a/b)
+elif operator=='%':
+    print(a%b)
+elif operator=='**':
+    print(a**b)
+else:
+    print("Invalid operation")
